e4067d633d897f7ced2bba0cc63b9d6a510a4cb0
ArieSaputra042 <1811500042@mahasiswa.atmaluhur.ac.id>
2021-01-13 23:04:59 +0700

    last

--- app/src/main/java/ac/id/atmaluhur/mhs/ajaapps/Halaman.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/ajaapps/Halaman.java
@@ -1,15 +1,16 @@
 package ac.id.atmaluhur.mhs.ajaapps;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Halaman extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.halaman_produk);
 
     }
 }
